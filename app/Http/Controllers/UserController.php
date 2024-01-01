d7b252942b0a531bcd3938a99cd2d35d84ec6bdd
Rasulov Akmal <akmal.rasulov@megafon.tj>
2024-01-01 23:04:10 +0500

    добавил typehint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,9 @@ use App\Http\Requests\UserStoreRequest;
 use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
 use App\Services\UserService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Response;
 
 class UserController extends Controller
 {
@@ -16,60 +18,60 @@ class UserController extends Controller
         $this->userService = $userService;
     }
 
-    public function index(Request $request)
+    public function index(Request $request): Response
     {
         return inertia()->render('Users/Index', $this->userService->index($request));
     }
 
-    public function create()
+    public function create(): Response
     {
         return inertia()->render('Users/Create', $this->userService->create());
     }
 
-    public function store(UserStoreRequest $request)
+    public function store(UserStoreRequest $request): RedirectResponse
     {
         $this->userService->store($request);
 
         return redirect()->route('users.index')->with('success', 'Успешно!');
     }
 
-    public function edit(User $user)
+    public function edit(User $user): Response
     {
         return inertia()->render('Users/Edit', $this->userService->edit($user));
     }
 
-    public function update(UserUpdateRequest $request, User $user)
+    public function update(UserUpdateRequest $request, User $user): RedirectResponse
     {
         $this->userService->update($request, $user);
 
         return back()->with('success', 'Успешно!');
     }
 
-    public function updateAvatar(UpdateAvatarRequest $request, User $user)
+    public function updateAvatar(UpdateAvatarRequest $request, User $user): RedirectResponse
     {
         $this->userService->updateAvatar($request, $user);
 
         return back()->with('success', 'Успешно!');
     }
 
-    public function destroy(User $user)
+    public function destroy(User $user): RedirectResponse
     {
         $this->userService->destroy($user);
 
         return redirect()->route('users.index')->with('success', 'Успешно!');
     }
 
-    public function restore(User $user)
+    public function restore(User $user): RedirectResponse
     {
         $this->userService->restore($user);
 
         return back()->with('success', 'Успешно!');
     }
 
-    public function assignRole(User $user)
+    public function assignRole(User $user): RedirectResponse
     {
         $userRoles = $user->getRoleNames();
-        if (count($userRoles) >= 1) {
+        if (count($userRoles) >= 1 && request('checked')) {
             return back()->with('warning', 'У пользователя может быть только одна роль!');
         }
 
@@ -78,7 +80,7 @@ class UserController extends Controller
         return back()->with('success', 'Успешно!');
     }
 
-    public function assignPermission(User $user)
+    public function assignPermission(User $user): RedirectResponse
     {
         $this->userService->assignPermission($user);
 
